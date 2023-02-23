f3b7f652ae857182fd678552de196652d1aa766b
kjh302903 <kjh302903@gmail.com>
2023-02-23 11:52:03 +0900

    Routing 추가 & SearchPage 완

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components'
 
 const Nav = () => {
 
   const [show, setShow] = useState(false);
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if(window.scrollY > 50) {
-        setShow(true);
-      }
-      else {
-        setShow(false);
-      }
-    })
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', () => {});
+      window.removeEventListener('scroll', handleScroll);
     }
   }, []);
+
+  const handleScroll = () => {
+    if(window.scrollY > 50) {
+      setShow(true);
+    }
+    else {
+      setShow(false);
+    }
+  }
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
   
   return (
     <NavWrapper show = {show}>
@@ -28,6 +39,17 @@ const Nav = () => {
           onClick={() => window.location.href = '/'}
         />
       </Logo>
+      {pathname === '/' 
+        ? (<Login>Login</Login>)
+        : (<Input 
+            className='nav__input'
+            type='text'
+            placeholder = '검색해주세요.'
+            value = {searchValue}
+            onChange = {handleChange}
+          />)
+        
+      }
     </NavWrapper>
   )
 }
@@ -61,4 +83,30 @@ const Logo = styled.a`
     display: block;
     width: 100%;
   }
-`
+`;
+
+const Login = styled.a`
+  background-color: rgba(0,0,0,.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: gray;
+    border-color: transparent;
+  }
+`;
+
+const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0,0,0,.582);
+  border-radius: 5px;
+  color: white;
+  padding: 5px;
+  border: none;
+`;
