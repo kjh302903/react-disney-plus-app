264cd72df16d263509679aa17a013fd2bb7a16bc
kjh302903 <kjh302903@gmail.com>
2023-02-23 14:11:26 +0900

    Detail page 완

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  let {movieId} = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`/movie/${movieId}`);
+      setMovie(response.data);
+    }
+    fetchData();
+  }, [movieId]);
+  
+  if(!movie) return null;
+
   return (
-    <div>DetailPage</div>
+    <section>
+      <img
+        className='modal__poster-img'
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+        alt='img'
+      />
+    </section>
   )
 }
 
