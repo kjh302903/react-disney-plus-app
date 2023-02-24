5f954e641aceb4de37003e930a6b6f01d2411b57
kjh302903 <kjh302903@gmail.com>
2023-02-24 12:04:40 +0900

    useOnClickOutside 훅 추가

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 import './MovieModal.css'
 
 const MovieModal = ({
@@ -10,11 +11,18 @@ const MovieModal = ({
   first_air_date,
   vote_average,
   setModalOpen
-}) => {
+  }) => {
+
+   const ref = useRef();
+
+   useOnClickOutside(ref, () => {
+    setModalOpen(false);
+   });
+
   return (
     <div className='presentation' role='presentation'>
       <div className='wrapper-modal'>
-        <div className='modal'>
+        <div className='modal' ref = {ref}>
           <span
             onClick={() => setModalOpen(false)}
             className= 'modal-close'
